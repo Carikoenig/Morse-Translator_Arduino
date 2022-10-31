0e18ce5272187f7f722f0828b8c413b932e1bd8a
Carina <clkoenig96@web.de>
2022-10-31 16:20:35 +0100

    Measure Button Press or Not Press Duration

--- Morse_arduino/Morse_arduino2/Morse_arduino2.ino
+++ Morse_arduino/Morse_arduino2/Morse_arduino2.ino
@@ -2,8 +2,12 @@ int buttonPin = 11;
 int ledPin = 2;
 int buttonState;
 // setup a timer to measure the length of the button press
-unsigned long timer = 0;
-int finalTimerValue = 0;
+unsigned long timer;
+//int finalTimerValue = 0;
+unsigned long lastNotPressedTime = 0;
+unsigned long lastPressedTime = 0;
+unsigned long duration = 0;
+
 
 void setup() {
   pinMode(buttonPin, INPUT);
@@ -14,18 +18,21 @@ void setup() {
 void loop() {
   // read whether button is pressed
   buttonState = digitalRead(buttonPin);
-
+  // track the since program started to run
+   timer = millis();
+  
   if (buttonState == HIGH) {
     // track the time of button press
-    timer = millis();
+    lastPressedTime = millis();
+    duration = lastPressedTime - lastNotPressedTime;
     // Light the LED
     digitalWrite(ledPin, HIGH);
     // send info through serial
     Serial.println("1");
-    Serial.println(timer);
+    Serial.println(duration);
   }
   if (buttonState == LOW) {
-    finalTimerValue = timer;
+    //finalTimerValue = timer;
     //String pressLength;
     // classify the length of the button press
     // a long press is 2 seconds long in morse code
@@ -39,13 +46,14 @@ void loop() {
     Serial.println("0 FTValue: "+ finalTimerValue);
     Serial.println("0 timer: " + timer);
     */
-
     // reset timer
-    timer = 0;  
+    lastNotPressedTime = millis();  
+    duration = lastNotPressedTime - lastPressedTime;
     // will be needed later to split words and sentences according to length of not pressing the button
     digitalWrite(ledPin, LOW);  // already handled this in the butpresss function?
     Serial.println("0");
-    Serial.println(finalTimerValue);
+    Serial.println(duration);
   }
   delay(50);
+  
 }
